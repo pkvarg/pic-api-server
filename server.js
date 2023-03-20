6f6c9074796bc55a8ace5bd691c914e4d29649b9
peter varga <pkvarg@yahoo.se>
2023-03-20 08:32:41 +0100

    cors

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const app = express()
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
+      // 'http://localhost:5173',
+      // 'http://localhost:3000',
       'https://pictusweb.sk',
     ],
   })
