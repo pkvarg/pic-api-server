7405d8855f2124677738f199b3fcbc9ee4fea83a
peter varga <pkvarg@yahoo.se>
2023-08-21 04:18:51 +0200

    CRUD dvl orders

--- controllers/dvlOrdersController.js
+++ controllers/dvlOrdersController.js
@@ -8,15 +8,32 @@ const dvlOrdersCont = asyncHandler(async (req, res) => {
     const orderData = req.body
     const order = new dvlOrders(orderData)
     await order.save()
-
-    res.status(201).json('order created')
+    res.status(201).json('OK')
   } else if (req.method === 'GET') {
-    console.log('GET')
-  } else if (req.method === 'PATCH') {
-    console.log('PATCH')
-  } else if (req.method === 'DELETE') {
-    console.log('DELETE')
+    const orders = await dvlOrders.find({})
+    res.status(200).json(orders)
   }
 })
 
-export { dvlOrdersCont }
+const dvlOrdersSingle = asyncHandler(async (req, res) => {
+  const id = req.params.id
+  if (req.method === 'GET') {
+    const order = await dvlOrders.findById(id)
+    res.status(200).json(order)
+  } else if (req.method === 'PATCH') {
+    const order = await dvlOrders.findById(id)
+    const { address, description, name, phone, title } = req.body
+    order.address = address
+    order.description = description
+    order.name = name
+    order.phone = phone
+    order.title = title
+    const savedOrder = await order.save()
+    res.status(200).json(savedOrder)
+  } else if (req.method === 'DELETE') {
+    const order = await dvlOrders.deleteOne({ _id: id })
+    res.status(200).json('OK')
+  }
+})
+
+export { dvlOrdersCont, dvlOrdersSingle }
--- routes/dvlOrdersRouter.js
+++ routes/dvlOrdersRouter.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import { dvlOrdersCont } from '../controllers/dvlOrdersController.js'
+import { dvlOrdersSingle } from '../controllers/dvlOrdersController.js'
 
 const router = express.Router()
 
 router.post('/dvl/orders', dvlOrdersCont)
 router.get('/dvl/orders', dvlOrdersCont)
-router.patch('/dvl/orders', dvlOrdersCont)
-router.delete('/dvl/orders', dvlOrdersCont)
+router.get('/dvl/orders/:id', dvlOrdersSingle)
+router.patch('/dvl/orders/:id', dvlOrdersSingle)
+router.delete('/dvl/orders/:id', dvlOrdersSingle)
 
 export default router
