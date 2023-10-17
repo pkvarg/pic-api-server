e7645ec4d8ee5e394532ad113d33e19830e2045d
peter varga <pkvarg@yahoo.se>
2023-10-17 15:14:07 +0200

    crud cba blog ok

--- controllers/cbaController.js
+++ controllers/cbaController.js
@@ -17,4 +17,67 @@ const createBlog = asyncHandler(async (req, res) => {
   res.status(201).json(createdNewCbaBlog)
 })
 
-export { createBlog }
+// GET api/cba/blogs
+
+const getAllBlogs = asyncHandler(async (req, res) => {
+  const blogs = await cbaBlog.find({})
+  res.json(blogs)
+})
+
+// GET api/cba/blogs/:id
+
+const getSingleBlog = asyncHandler(async (req, res) => {
+  const blog = await cbaBlog.findById(req.params.id)
+  if (blog) {
+    res.json(blog)
+  } else {
+    res.status(404)
+    throw new Error('Blog not found')
+  }
+})
+
+// PUT api/cba/blogs/update/:id
+
+const updateSingleBlog = asyncHandler(async (req, res) => {
+  const { title, category, media, text } = req.body
+  const blog = await cbaBlog.findById(req.params.id)
+  if (blog) {
+    blog.title = title
+    blog.category = category
+    blog.media = media
+    blog.text = text
+
+    const savedBlog = await blog.save()
+
+    res.json(savedBlog)
+  } else {
+    res.status(404)
+    throw new Error('Blog not found')
+  }
+})
+
+// DELETE api/cba/blogs/:id
+
+const deleteSingleBlog = asyncHandler(async (req, res) => {
+  const blog = await cbaBlog.findById(req.params.id)
+  if (blog) {
+    try {
+      await blog.deleteOne()
+      res.json({ message: 'Blog removed' })
+    } catch (error) {
+      res.status(404)
+      throw new Error('Blog not found')
+    }
+  } else {
+    res.status(404)
+    throw new Error('Blog not found')
+  }
+})
+
+export {
+  createBlog,
+  getAllBlogs,
+  getSingleBlog,
+  updateSingleBlog,
+  deleteSingleBlog,
+}
--- routes/cbaRouter.js
+++ routes/cbaRouter.js
@@ -1,8 +1,18 @@
 import express from 'express'
-import { createBlog } from '../controllers/cbaController.js'
+import {
+  createBlog,
+  getAllBlogs,
+  getSingleBlog,
+  updateSingleBlog,
+  deleteSingleBlog,
+} from '../controllers/cbaController.js'
 
 const router = express.Router()
 
 router.post('/blog', createBlog)
+router.get('/blogs', getAllBlogs)
+router.get('/blogs/:id', getSingleBlog)
+router.put('/blogs/update/:id', updateSingleBlog)
+router.delete('/blogs/:id', deleteSingleBlog)
 
 export default router
